83d26c9edf86c1f03e222b591223e344003b867e
Mr-Zad <91231446+Mr-Zad@users.noreply.github.com>
2024-02-20 00:57:12 +0800

    upload

---
+++ run_recbole.py
@@ -0,0 +1,18 @@
+
+
+import argparse
+
+from recbole.quick_start import run_recbole
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--model', '-m', type=str, default='MASAN', help='name of models')
+    parser.add_argument('--dataset', '-d', type=str, default='ml-1m', help='name of datasets: ml-1m/yelp/Amazon_Books')
+    parser.add_argument('--config_files', type=str, default=None, help='config files')
+
+    args, _ = parser.parse_known_args()
+
+    config_file_list = args.config_files.strip().split(' ') if args.config_files else None
+    run_recbole(model=args.model, dataset=args.dataset, config_file_list=config_file_list)
